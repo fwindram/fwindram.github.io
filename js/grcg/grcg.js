8b4855fa0e67f36a26a46f388b06d5388ecc5fc9
Francis Windram <francis.windram@hotmail.co.uk>
2021-06-06 02:22:05 +0100

    Add grcg v0.0.1

---
+++ js/grcg/grcg.js
@@ -0,0 +1,92 @@
+const difficulties = ["Normal", "Elite", "Nightmare", "Any"]
+
+const characters = ["Ao Bai", "Crown Prince", "Qing Yan", "Lei Luo", "Any Character"]
+
+// [challenge, weight]
+// Any challenge with a weight of 1 will stop sampling after it is picked
+const challenges = [
+  ["Normal Run", 1],
+  ["Speedrun", 0.4],
+  ["No Dash", 0.5],
+  ["VR Mode: Only Dash", 1],
+  ["No Craftsman", 0.3],
+  ["No Kermit", 0.3],
+  ["No Vaults", 0.3],
+  ["All Vaults", 0.1],
+  ["No Cursed Chests", 0.3],
+  ["All Cursed Chests", 0.3],
+  ["No Secondary Skills", 0.3],
+  ["No Primary Skills", 0.3],
+  ["No Scrolls", 1],
+  ["No Acensions", 1],
+  ["No Revive", 0.3],
+  ["No Manual Reloads", 0.2],
+  ["No Elemental Weapons", 0.2],
+  ["No Insurance: Try not to break anything", 0.2],
+  ["Butterfingers: Always Switch Weapons", 0.2],
+  ["Sticky Fingers: Never Switch Weapons", 0.2],
+  ["Rebind Hell: Invert Movement Bindings", 1],
+  ["NES Mode: Lowest Resolution", 1],
+  ["Foundry Only", 0.5],
+  ["Pistols", 0.5],
+  ["Submachineguns", 0.3],
+  ["Rifles", 0.3],
+  ["Shotguns", 0.3],
+  ["Snipers", 0.3],
+  ["Launchers", 0.3],
+  ["Injectors", 0.3],
+  ["Melee", 0.3],
+  ["Only Fire Weapons", 0.3],
+  ["Only Corrosion Weapons", 0.3],
+  ["Only Lightning Weapons", 0.3],
+  ["Student Loan: Spend money ASAP", 0.5],
+  ["No Gemini", 0.1],
+  ["OGemini: No reforging", 0.1],
+  ["NoUI: Press F10 to play without UI", 0.7],
+  ["OGemini: No reforging", 0.1],
+  ["Backwards movement only", 0.4],
+  ["Only Turn Left", 0.5],
+  ["Only Turn Right", 0.5],
+  ["Left Handed Mouse", 0.5],
+  ["Change Language", 0.6],
+]
+
+// Why in the name of all that is holy do I need to write fisher-yates again just to shuffle an array?
+function shuffle(array) {
+  let curridx = array.length,  randidx;
+  while (0 !== curridx) {
+    randidx = Math.floor(Math.random() * curridx);
+    curridx--;
+    [array[curridx], array[randidx]] = [
+      array[randidx], array[curridx]];
+  }
+  return array;
+}
+
+
+function generateRun() {
+  // let outstr = ""
+  let difficulty = difficulties[Math.floor(Math.random() * difficulties.length)]
+  // console.log(difficulty)
+  let character = characters[Math.floor(Math.random() * characters.length)]
+  // console.log(character)
+
+  let challenge_list = []
+  let challenges_copy = challenges
+  // We need to shuffle the array because js arrays do not have a pop command that will take an index to pop.
+  // I'm sure all this would be a lot easier if I just learned node.js but that feels ludicrous!
+  challenges_copy = shuffle(challenges_copy)
+
+  let challenge = []
+  let counter = 0
+  let randfloat = 1
+  while (counter < randfloat) {
+    challenge = challenges_copy.pop()
+    challenge_list.push(challenge[0])
+    counter += challenge[1]
+    randfloat = Math.random()
+  }
+  // console.log(challenge_list)
+  document.getElementById('playerDisplay').innerHTML = "".concat(character, " on ", difficulty, " difficulty")
+  document.getElementById('challengeDisplay').innerHTML = challenge_list.join("<br>")
+}
